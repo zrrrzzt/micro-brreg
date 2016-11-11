ba639b3b0e42a2ca1104dea5d61933764247a2fe
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-11 22:01:28 +0100

    Switches to brreg promises interface

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ module.exports = async (request, response) => {
   const { query } = parse(request.url, true)
   const q = query.query || ''
 
-  return await brreg({query: q}, (error, data) => {
-    if (error) {
-      send(response, 500, error.message)
-    } else {
+  return await brreg({query: q})
+    .then(data => {
       response.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'})
       response.write(JSON.stringify(data.entries))
       response.end()
-    }
-  })
+    })
+    .catch(error => {
+      send(response, 500, error.message)
+    })
 }
