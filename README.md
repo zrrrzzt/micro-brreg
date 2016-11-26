4b6dced20b46389b5359213363ad5cf5edb2afb1
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-11-26 22:06:39 +0100

    Complete rewrite

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # micro-brreg
 microservice for brreg
+
+```sh
+$ curl -d '{"query": "pythonia"}' -v http://localhost:3000
+```
+
+## License
+[MIT](LICENSE)
--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 'use strict'
 
-const { parse } = require('url')
-const { send } = require('micro')
+const { json, send } = require('micro')
 const brreg = require('brreg')
 
 module.exports = async (request, response) => {
-  const { query } = parse(request.url, true)
-  const q = query.query || ''
-
-  return await brreg({query: q})
-    .then(data => {
-      response.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'})
-      response.write(JSON.stringify(data.entries))
-      response.end()
-    })
-    .catch(error => {
-      send(response, 500, error.message)
-    })
+  const data = await json(request)
+  const result = await brreg(data)
+  send(response, 200, result.entries)
 }
