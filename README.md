eba6ba6bfe35fb7bc6d7741c1e0b3bada5b753d8
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-12 16:18:07 +0100

    Switches to got and brreg API (major)

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # micro-brreg
 microservice for brreg
 
+## API
+
+### GET
+
+```
+$ curl -v http://localhost:3000?organisasjonsnummer=974760673
+```
+
+### POST
+
 ```sh
-$ curl -d '{"query": "pythonia"}' -v http://localhost:3000
+$ curl -d '{"organisasjonsnummer": "974760673"}' -v http://localhost:3000
 ```
 
 ## License
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 'use strict'
 
+const readFileSync = require('fs').readFileSync
+const { parse } = require('url')
+const marked = require('marked')
 const { json, send } = require('micro')
-const brreg = require('brreg')
+const getEnhet = require('./lib/get-enhet')
+const getUnderenhet = require('./lib/get-underenhet')
 
 module.exports = async (request, response) => {
-  const data = await json(request)
-  const result = await brreg(data)
-  send(response, 200, result.entries)
+  const {pathname, query} = await parse(request.url, true)
+  const data = request.method === 'POST' ? await json(request) : query
+
+  if (data.organisasjonsnummer && data.organisasjonsnummer.length > 0) {
+    const enhet = await getEnhet(data.organisasjonsnummer)
+    const underenhet = await getUnderenhet(data.organisasjonsnummer)
+
+    const result = enhet || underenhet
+    send(response, 200, result || {})
+  } else {
+    const readme = readFileSync('./README.md', 'utf-8')
+    const html = marked(readme)
+    send(response, 200, html)
+  }
 }
