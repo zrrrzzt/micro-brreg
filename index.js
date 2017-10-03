5ee0bf338e095f7b3fb58b923f6ed5663b36b567
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-10-03 08:56:03 +0200

    Updates Node. Switches to markdown-it. Removes logging of 404

--- index.js
+++ index.js
@@ -2,25 +2,25 @@
 
 const readFileSync = require('fs').readFileSync
 const { parse } = require('url')
-const marked = require('marked')
+const md = require('markdown-it')()
 const { json, send } = require('micro')
 const getEnhet = require('./lib/get-enhet')
 
 module.exports = async (request, response) => {
   const {query} = await parse(request.url, true)
   const data = request.method === 'POST' ? await json(request) : query
-
   if (data.organisasjonsnummer && data.organisasjonsnummer.length > 0) {
-    const enhet = await getEnhet(data.organisasjonsnummer, 'enhet')
-    const underenhet = await getEnhet(data.organisasjonsnummer, 'underenhet')
+    try {
+      const [enhet, underenhet] = await Promise.all([getEnhet(data.organisasjonsnummer, 'enhet'), getEnhet(data.organisasjonsnummer, 'underenhet')])
+      const result = enhet || underenhet
+      const code = result ? 200 : 404
 
-    const result = enhet || underenhet
-    const code = result ? 200 : 404
-
-    send(response, code, result || {error: 'Not found'})
+      send(response, code, result || {error: 'Not found'})
+    } catch (error) {
+      send(response, 500, error)
+    }
   } else {
     const readme = readFileSync('./README.md', 'utf-8')
-    const html = marked(readme)
-    send(response, 200, html)
+    send(response, 200, md.render(readme))
   }
 }
